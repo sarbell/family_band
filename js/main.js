dbe601b251ee9045b962107c08577a585e2222b0
sarbell <96sarahbell@gmail.com>
2020-11-02 00:18:00 -0700

    update

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
 $(window).scroll(function(){
 	$('nav').toggleClass('scrolled', $(this).scrollTop() > 50);
 });
+
+
+let hamburger = document.querySelector('.menu_icon');
+hamburger.addEventListener('click', function (e){
+	if(hamburger.classList.contains('fa-bars')){
+		hamburger.classList.remove('fa-bars')
+		hamburger.classList.add('fa-times')
+	}
+	else{
+		hamburger.classList.remove('fa-times')
+		hamburger.classList.add('fa-bars')
+	}
+})
